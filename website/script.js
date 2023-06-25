3af7c030c56388ded7dda8e060166486b17c981c
hasibur815 <136497079+hasibur815@users.noreply.github.com>
2023-06-25 15:28:48 +0530

    Add files via upload
    index.html and images added

---
+++ website/script.js
@@ -0,0 +1,14 @@
+// script.js
+
+// Custom JavaScript code
+// Add your functions, event listeners, and other JavaScript logic here
+
+// Example: Function to handle a button click event
+function handleClick() {
+  // Code to handle the button click
+  console.log('Button clicked!');
+}
+
+// Example: Adding an event listener to a button
+const button = document.querySelector('#my-button');
+button.addEventListener('click', handleClick);
